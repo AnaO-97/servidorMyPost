3664cdafca82dc253d0ab7f9f5fc63fc23c12548
ana.orozco@autonoma.edu.co <ana.orozco@autonoma.edu.co>
2024-04-04 09:53:06 -0500

    DB_PROTOCOL added

--- src/db.js
+++ src/db.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const { Sequelize } = require("sequelize");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { DB_PROTOCOL, DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const postModel     = require("./models/Post");
 const usersModel    = require("./models/User");
 const favoriteModel = require("./models/Favorite");
 
 const database = new Sequelize(
-    `postgres://${ DB_USER }:${ DB_PASSWORD }@${ DB_HOST }/${ DB_NAME }`,
+    `${ DB_PROTOCOL }://${ DB_USER }:${ DB_PASSWORD }@${ DB_HOST }/${ DB_NAME }`,
     { logging: false }
 )
 
