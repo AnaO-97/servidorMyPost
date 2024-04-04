2671982336f3088b5f84538a8439acb3789fdddb
ana.orozco@autonoma.edu.co <ana.orozco@autonoma.edu.co>
2024-04-04 11:55:48 -0500

    Cambio de PORT_SERVER por PORT

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const app = require("./src/app");
 const { database } = require("./src/db");
-const { PORT_SERVER } = process.env || 3000;
+const { PORT } = process.env || 3000;
 
 // force : elimina
 // alter : actualiza
 
 database.sync({ alter: true }).then((res)=>{
-    app.listen(PORT_SERVER, ()=>{
+    app.listen(PORT, ()=>{
         console.log(`Server running on port ${ PORT_SERVER } XD`);
     });
 }).catch((err)=>{
