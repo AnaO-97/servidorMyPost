81d9a0c06522b0c27168a931618b0cc9093fd556
ana.orozco@autonoma.edu.co <ana.orozco@autonoma.edu.co>
2024-04-05 14:44:17 -0500

    Volviendo a la conección con la DB

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const { database } = require("./src/db");
 // force : elimina
 // alter : actualiza
 
-// database.sync({ alter: true }).then((res)=>{
+database.sync({ alter: true }).then((res)=>{
     app.listen(PORT, "0.0.0.0", ()=>{
         console.log(`Server running on port ${ PORT } XD`);
     });
-// }).catch((err)=>{
-//     console.log(`SRC/APP error ${ err }`)
-// });
+}).catch((err)=>{
+    console.log(`SRC/APP error ${ err }`)
+});
